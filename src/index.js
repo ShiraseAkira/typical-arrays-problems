931c5a43267ea7123cd61b74d3940ff534acef90
ShiraseAkira <justsaynya@gmail.com>
2021-08-16 21:55:04 +0300

    init: solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
 exports.min = function min (array) {
+  if (!arguments.length || !array.length)
   return 0;
+
+  let minElem = array[0];
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] < minElem) {
+      minElem = array[i];
+    }
+  }
+  return minElem
 }
 
 exports.max = function max (array) {
+  if (!arguments.length || !array.length)
   return 0;
+
+  let maxElem = array[0];
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] > maxElem) {
+      maxElem = array[i];
+    }
+  }
+  return maxElem
 }
 
 exports.avg = function avg (array) {
+  if (!arguments.length || !array.length)
   return 0;
+
+  let sum = array[0];
+  for (let i = 1; i < array.length; i++) {
+    sum += array[i]
+  }
+  return sum / array.length
 }
